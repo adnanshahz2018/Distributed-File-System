4de722ef3e1969a421ee4ae822415b05ec30b13b
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-16 11:19:56 +0500

    Used for Saving File Info: name and tag

---
+++ src/FileTable.java
@@ -0,0 +1,23 @@
+
+public class FileTable 
+{		
+	String filename ;
+	String tag ;
+	
+}
+
+/*
+------------------------
+filename: string
+this variable holds the name of the file uploaded by the user 
+e.g. userdata.txt
+
+------------------------
+tag: string
+this variable has its own special name assigned by the "Root" Server
+e.g. 		userdata.0123456789.txt
+
+------------------------
+
+*/
+
